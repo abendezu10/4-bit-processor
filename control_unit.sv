623a90ca18de51d1363e5df35234e7baccdf4fca
Alexander Bendezu <abendezu385@gmail.com>
2024-11-16 01:15:58 -0500

    sds

--- control_unit.sv
+++ control_unit.sv
@@ -1,6 +1,5 @@
 module control_unit(
 
-  input [3:0] instruction,
   input clk,
   
 
@@ -9,7 +8,9 @@ module control_unit(
   output [1:0] number
   );
 
+
   // 1. Fetches the isntruction from memory
+
   // 2. Decodes instruction, in this case, 1 bit for register, 1 bit for the
   // operation, 2 bit for the number.
 
@@ -18,11 +19,25 @@ module control_unit(
   localparam CU_DECODE_STATE = 2'd2;
 
   logic state = CU_IDLE_STATE;
+  logic [3:0] pc = 4'd0;
+  logic [3:0] instr;
 
 
   // State Transitioning
   always_ff @ (posedge clk) begin
     
-    case()
+    case(state)
+      CU_FETCH_STATE: begin
+        instr_mem mem_b (
+            .instr_addr(pc),
+            .data_o(instr)
+          );
+          if(instr != NULL) begin
+            state = CU_DECODE_STATE;
+          end
 
-  end
+
+      end
+
+
+    end
---
+++ instr_mem.sv
@@ -0,0 +1,19 @@
+module instr_mem (
+  input logic [3:0] instr_addr,
+
+  output logic [3:0] data_o  
+  );
+
+   logic [3:0] memory [0:15];  
+
+  initial begin
+    $readmemb("machine_code.bin", memory);
+  end
+
+
+  always_comb begin 
+
+      data_o <= memory[instr_addr]
+  end
+endmodule
+
